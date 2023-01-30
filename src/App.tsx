18e2fbc679bc00d0a62f71d03dfc763e8c3b9d4f
Sergey Rudenko <rudenko-serg@ya.ru>
2023-01-30 15:44:54 +0300

    Add react router dom, add lazy and suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import "./index.scss"
-import {Counter} from "./components/Counter";
+import {Route, Routes, Link} from "react-router-dom";
+import {AboutAsync} from "./pages/About/About.async";
+import {MainAsync} from "./pages/Main/Main.async";
 
 export const App = () => {
     return (
         <div className="app">
-            <Counter />
+            <Link to="/">Main</Link> <Link to="/about">About</Link>
+            <Suspense fallback={<div>...loading</div>}>
+                <Routes>
+                    <Route path="/about" element={<AboutAsync />}/>
+                    <Route path="/" element={<MainAsync />}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };

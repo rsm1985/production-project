4c2a5426fd24c26f41d0787a3fa0cbd664a1a666
Sergey Rudenko <rudenko-serg@ya.ru>
2023-01-31 13:52:01 +0300

    Add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
-import React, {Suspense} from 'react';
+import React, {Suspense, useContext, useState} from 'react';
 import "./styles/index.scss"
 import {Route, Routes, Link} from "react-router-dom";
 import {AboutAsync} from "./pages/About/About.async";
 import {MainAsync} from "./pages/Main/Main.async";
+import {Theme} from "./theme/ThemeContext";
+import {useTheme} from "./theme/UseTheme";
+import {classNames} from "./helpers/classNames/classNames";
+
 
 export const App = () => {
+    const {
+        theme,
+        toggleTheme
+    } = useTheme()
+
     return (
-        <div className="app">
+        <div className={classNames("app",
+            {
+            }, [theme])
+        }>
             <Link to="/">Main</Link> <Link to="/about">About</Link>
+            <button onClick={() => toggleTheme()}>Toggle theme</button>
             <Suspense fallback={<div>...loading</div>}>
                 <Routes>
-                    <Route path="/about" element={<AboutAsync />}/>
-                    <Route path="/" element={<MainAsync />}/>
+                    <Route path="/about" element={<AboutAsync/>}/>
+                    <Route path="/" element={<MainAsync/>}/>
                 </Routes>
             </Suspense>
         </div>
---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,7 @@
+type Mods = Record<string, boolean | string>
+
+export const classNames = (cls: string, mods?: Mods, additional?: string[]):string => [
+    cls,
+    Object.keys(mods).filter((key)=> mods[key]).join(" "),
+    ...additional
+].join(" ")

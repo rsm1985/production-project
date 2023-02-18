c188ba1fbde35ef62dd73440995672273a61836e
Sergey Rudenko <rudenko-serg@ya.ru>
2023-02-18 18:44:45 +0300

    Add jest

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,18 @@
+import {classNames} from './classNames';
+
+describe('classNames', () => {
+	test('with first param', () => {
+		expect(classNames('class')).toBe('class');
+	});
+	test('with first param and mod', () => {
+		expect(classNames('class', {mod1: true})).toBe('class mod1');
+	});
+	test('with all params', () => {
+		expect(classNames('class', {mod1: true}, ['additional-1', 'additional-2'] ))
+			.toBe('class mod1 additional-1 additional-2');
+	});
+	test('with all params and on mod is false', () => {
+		expect(classNames('class', {mod1: true, mod2: false}, ['additional-1', 'additional-2'] ))
+			.toBe('class mod1 additional-1 additional-2');
+	});
+});
